61da82fa60e2c7eb14c86e8029017024cfa07319
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-02 18:28:59 -0300

    calculadora finished

--- Calculadora/src/app/calculadora/calculadora.service.ts
+++ Calculadora/src/app/calculadora/calculadora.service.ts
@@ -9,4 +9,16 @@ export class CalculadoraService {
   public somar(number1: number, number2: number): number {
     return number1 + number2;
   }
+
+  public subtrair(number1: number, number2: number): number {
+    return number1 - number2;
+  }
+
+  public multiplicar(number1: number, number2: number): number {
+    return number1 * number2;
+  }
+
+  public dividir(number1: number, number2: number): number {
+    return number1 / number2;
+  }
 }
--- Calculadora/src/app/calculadora/calculadora/calculadora.component.ts
+++ Calculadora/src/app/calculadora/calculadora/calculadora.component.ts
@@ -20,6 +20,27 @@ export class CalculadoraComponent implements OnInit {
     this.res = this.calculadoraService.somar(n1, n2);
   }
 
+  public subtrair(number1: string, number2: string): void {
+    let n1: number = parseFloat(number1);
+    let n2: number = parseFloat(number2);
+
+    this.res = this.calculadoraService.subtrair(n1, n2);
+  }
+
+  public multiplicar(number1: string, number2: string): void {
+    let n1: number = parseFloat(number1);
+    let n2: number = parseFloat(number2);
+
+    this.res = this.calculadoraService.multiplicar(n1, n2);
+  }
+
+  public dividir(number1: string, number2: string): void {
+    let n1: number = parseFloat(number1);
+    let n2: number = parseFloat(number2);
+
+    this.res = this.calculadoraService.dividir(n1, n2);
+  }
+
   get getResultado(): string | undefined {
     return this.res?.toString();
   }
