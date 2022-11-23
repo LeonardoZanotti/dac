906ab4a047abaa795fa2887f5268633ecea64a39
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-23 16:56:08 -0300

    estados crud

--- crud-pessoa/src/app/estado/estado.module.ts
+++ crud-pessoa/src/app/estado/estado.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ListarEstadoComponent } from './listar-estado/listar-estado.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { EditarEstadoComponent } from './editar-estado/editar-estado.component';
 import { InserirEstadoComponent } from './inserir-estado/inserir-estado.component';
-
-
+import { ListarEstadoComponent } from './listar-estado/listar-estado.component';
+import { EstadoService } from './services/estado.service';
 
 @NgModule({
   declarations: [
     ListarEstadoComponent,
     EditarEstadoComponent,
-    InserirEstadoComponent
+    InserirEstadoComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule, FormsModule],
+  providers: [EstadoService],
 })
-export class EstadoModule { }
+export class EstadoModule {}
--- crud-pessoa/src/app/estado/services/estado.service.ts
+++ crud-pessoa/src/app/estado/services/estado.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Estado } from './../../shared/models/estado.model';
+
+const LS_CHAVE: string = 'estados';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EstadoService {
+  constructor() {}
 
-  constructor() { }
+  listarTodos(): Estado[] {
+    const estados = localStorage[LS_CHAVE];
+    return estados ? JSON.parse(estados) : [];
+  }
+
+  inserir(estado: Estado): void {
+    const estados: Estado[] = this.listarTodos();
+    estado.id = new Date().getTime();
+    estados.push(estado);
+    localStorage[LS_CHAVE] = JSON.stringify(estados);
+  }
+
+  buscarPorId(id: number): Estado | undefined {
+    const estados: Estado[] = this.listarTodos();
+    return estados.find((estado) => (estado.id = id));
+  }
+
+  atualizar(estado: Estado): void {
+    const estados: Estado[] = this.listarTodos();
+    estados.forEach((obj, index, objs) => {
+      if (estado.id === obj.id) {
+        objs[index] = estado;
+      }
+    });
+    localStorage[LS_CHAVE] = JSON.stringify(estados);
+  }
+
+  remover(id: number): void {
+    let estados: Estado[] = this.listarTodos();
+    estados = estados.filter((estado) => estado.id !== id);
+    localStorage[LS_CHAVE] = JSON.stringify(estados);
+  }
 }
