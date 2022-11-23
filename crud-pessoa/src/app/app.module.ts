25534b7b4518409709bf92f246a76d6bf26d6481
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-23 11:34:52 -0300

    Pessoa Module

--- crud-pessoa/src/app/app.module.ts
+++ crud-pessoa/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoaModule } from './pessoa/pessoa.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, PessoaModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
