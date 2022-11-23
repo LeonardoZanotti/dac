1290ecf0da7566333c9d7dd477e46d9114ff8208
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-23 19:40:55 -0300

    masks and validations

--- crud-pessoa-melhorado/src/app/endereco/endereco.module.ts
+++ crud-pessoa-melhorado/src/app/endereco/endereco.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgxMaskModule } from 'ngx-mask';
+import { SharedModule } from './../shared/shared.module';
 import { EditarEnderecoComponent } from './editar-endereco/editar-endereco.component';
 import { InserirEnderecoComponent } from './inserir-endereco/inserir-endereco.component';
 import { ListarEnderecoComponent } from './listar-endereco/listar-endereco.component';
@@ -13,7 +15,13 @@ import { EnderecoService } from './services/endereco.service';
     InserirEnderecoComponent,
     EditarEnderecoComponent,
   ],
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    SharedModule,
+    NgxMaskModule.forRoot(),
+  ],
   providers: [EnderecoService],
 })
 export class EnderecoModule {}
