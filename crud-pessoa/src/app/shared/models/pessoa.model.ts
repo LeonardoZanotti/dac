2e98dd48cb8e1f61e989c28a86f9b2a206897f8b
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-23 16:04:04 -0300

    added birthday

--- crud-pessoa/src/app/shared/models/pessoa.model.ts
+++ crud-pessoa/src/app/shared/models/pessoa.model.ts
@@ -2,6 +2,7 @@ export class Pessoa {
   constructor(
     public id?: number,
     public nome?: string,
-    public idade?: number
+    public idade?: number,
+    public nascimento?: Date
   ) {}
 }
