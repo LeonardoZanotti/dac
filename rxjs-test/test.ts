3828dba555ea187eed4bedd61a5e343b173266ab
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-11-26 14:49:33 -0300

    rxjs test done

--- rxjs-test/test.ts
+++ rxjs-test/test.ts
@@ -1,3 +1,117 @@
-import { from } from 'rxjs';
+import { concat, count, filter, from, map, max, min, of, take } from 'rxjs';
 
+// from
+console.log('from');
 from(['Curitiba', 'SJP', 'Pinhais', 'CIC', 'Colombo']).subscribe((valor: string) => console.log(valor));
+
+// Observer with two dots
+let arr = ['Ibicaré', 'Joaçaba', 'Treza Tílias', 'Luzerna'];
+from(arr).subscribe({
+  next: (valor: string) => {
+    let x = 'Cidade: ' + valor;
+    console.log(x);
+  },
+  error: (valor: string) => console.log('Erro: ' + valor),
+  complete: () => console.log('Acabou'),
+});
+
+// Observer with functions
+from(arr).subscribe({
+  next(valor: string) {
+    let x = 'Cidade: ' + valor;
+    console.log(x);
+  },
+  error(valor: string) {
+    console.log('Erro: ' + valor);
+  },
+  complete() {
+    console.log('Acabou');
+  },
+});
+
+// of
+of('Curitiba', 200, 4.5, true).subscribe({
+  next: (valor: string) => {
+    let x = 'Valor: ' + valor;
+    console.log(x);
+  },
+  error: (valor: string) => console.log('Erro: ' + valor),
+  complete: () => console.log('Acabou'),
+});
+
+// pipe
+let valores = Array.from({ length: 10 }, (_ = 5, i = 1) => +_ * (i + 1));
+
+console.log('\nobs1');
+const obs1 = from(valores).pipe(filter((valor: number) => valor % 2 == 0));
+obs1.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs2');
+const obs2 = from(valores).pipe(
+  filter((valor: number) => valor % 2 == 0),
+  map((valor: number) => valor / 10)
+);
+obs2.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs3');
+const obs3 = from(valores).pipe(
+  filter((valor: number) => valor % 2 == 0),
+  map((valor: number) => valor / 10),
+  take(2)
+);
+obs3.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs4');
+const obs4 = from(valores).pipe(
+  filter((valor: number) => valor % 2 == 0),
+  count()
+);
+obs4.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs5');
+const obs5 = from(valores).pipe(count((valor: number) => valor % 2 == 0));
+obs5.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs6');
+const obs6 = from(valores).pipe(max());
+obs6.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs7');
+const obs7 = from(valores).pipe(min());
+obs7.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
+
+console.log('\nobs8');
+const obs8 = concat(obs2, obs3, obs4);
+obs8.subscribe({
+  next(valor: number) {
+    console.log(valor);
+  },
+});
